ce1f2f30f72736dc99f87ecc857f6bc71803b97f
alin <alin.voinea@eaudeweb.ro>
2025-06-25 14:23:52 +0300

    Release 1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,11 @@ Changelog
 =========
 
 
+1.1 - (2025-06-25)
+------------------
+- Fix release to include zcml files
+  [dobri1408, avoinea]
+
 1.0 - (2025-06-23)
 ------------------
 
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.volto.footer",
-    version="1.0",
+    version="1.1",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
